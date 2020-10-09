10daf89fac45638282a12fde8e1e4adb3a4fcf93
Alex Smith <alexsmith_540@yahoo.com>
2020-10-08 23:32:29 -0600

    Update main.js
    update build link to 0.0.4.1

--- guideJS/main.js
+++ guideJS/main.js
@@ -16,8 +16,8 @@ $(document).ready(()=>{
 class HandyGuide{
 	constructor(){
 		//////CHANGEME WHEN MINER UPDATES HAPPEN
-		this.zipDownloadLink = 'https://github.com/HandyMiner/HandyMiner-Goldshell-CLI/archive/0.0.4.zip';
-		this.tarDownloadLink = 'https://github.com/HandyMiner/HandyMiner-Goldshell-CLI/archive/0.0.4.tar.gz';
+		this.zipDownloadLink = 'https://github.com/HandyMiner/HandyMiner-Goldshell-CLI/archive/0.0.4.1.zip';
+		this.tarDownloadLink = 'https://github.com/HandyMiner/HandyMiner-Goldshell-CLI/archive/0.0.4.1.tar.gz';
 		//END CHANGEME
 
 		this.changeLanguages();
